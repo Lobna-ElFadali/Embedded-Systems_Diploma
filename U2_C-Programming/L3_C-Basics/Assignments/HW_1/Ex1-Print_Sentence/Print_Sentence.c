d2b6e75823d178dba8c6be1fcda464cd9231d2a7
Lobna_ElFadali <lobna.elfadali@gmail.com>
2022-11-02 22:34:41 +0000

    Ex1 .c file

---
+++ U2_C-Programming/L3_C-Basics/Assignments/HW_1/Ex1-Print_Sentence/Print_Sentence.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ File name   : Print_Sentence.c
+ Project     : Ex1-Print_Sentence
+ Created on  : 2 Nov 2022
+ Author      : Lobna_ElFadali
+ 
+ Description :
+ Write C Program to Print a Sentence.
+
+ ####################Console-Output####################
+ C Programming
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
